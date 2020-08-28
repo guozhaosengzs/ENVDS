5a12bacbaee35a4b14cf9c38038ba87550fc8cb6
guozhaosengzs <guozhaosengzs@yahoo.com>
2020-08-28 10:34:07 -0400

    activity1 finished

--- activity1/activity1_script.R
+++ activity1/activity1_script.R
@@ -1,2 +1,51 @@
 # Activity 1
+
+# Math Practice
+a = 2
+b = 3
+c = 5
+d = 7
+e = 11
+
+vec1 = c(a, b, c ,d)
+vec1
+
+vec2 = vec1 - b
+vec2
+
+vec3 = vec2 * 11 - vec1
+vec3
+
+vec4 = vec3 * vec1
+vec4
+
+
+#####################
+## Tutorials Below ## 
+#####################
 print("hello R world")
+
+#remember this is a comment so it won't do anything. R just ignores it.
+#you need to use these to document code and write notes
+#6 raised to the 6 power
+6^6
+
+
+# 5 plus 210
+5+210
+
+#3 minus 10
+3-10
+
+#name my number
+a <- 2446
+#multiply my number by 5
+5*a
+#divide my number by 2
+a/2
+
+#make a vector of numbers
+b <- c(2446,40,10000)
+#divide all numbers by 2
+b/2
+
