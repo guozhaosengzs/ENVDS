bda39c2cc6442cf62baa794022f41f1ff71058ef
guozhaosengzs <guozhaosengzs@yahoo.com>
2020-10-15 21:23:55 -0400

    Q7

--- activity6/activity6_script.R
+++ activity6/activity6_script.R
@@ -2,6 +2,7 @@
 library(sp)
 library(rgdal)
 library(dplyr)
+library(tidyverse)
 
 ####Q1####
 g1966 <- readOGR("C:\\Users\\guozh\\Desktop\\ENVDS\\activity6\\GNPglaciers\\GNPglaciers_1966.shp")
@@ -30,4 +31,21 @@ g2015@data$GLACNAME <- ifelse(g2015@data$GLACNAME == "North Swiftcurrent Glacier
                               ifelse(   g2015@data$GLACNAME ==  "Miche Wabun", 
                                         "Miche Wabun Glacier",
                                         as.character(g2015@data$GLACNAME)))
+
+
+
+####Q3####
+####Q4####
+####Q5####
+gdf66 <- data.frame(GLACNAME = g1966@data$GLACNAME, area66 = g1966@data$Area1966)
+gdf15 <- data.frame(GLACNAME = g2015@data$GLACNAME, area15 = g2015@data$Area2015)
+gAll <- full_join(gdf66,gdf15, by="GLACNAME")
+
+####Q6####
+####Q7####
+gAll$gdiff <- ((gAll$area66-gAll$area15)/gAll$area66)*100
+
+gl.chg <- ggplot(gAll, aes(x = area66, y = gdiff)) + geom_point(size=2)
+gl.chg
+
 
