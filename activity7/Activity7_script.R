e764ab80ab9f7de7c9d103c0dec2b28e0e79ae84
guozhaosengzs <guozhaosengzs@yahoo.com>
2020-10-23 19:26:32 -0400

    done !

--- activity7/Activity7_script.R
+++ activity7/Activity7_script.R
@@ -7,10 +7,11 @@ data1$MapDate <- anydate(data1$MapDate)
 data2 <- data1[, c(1, 3:8)]
 data3 <- melt(data2, id.vars = "MapDate", measure.vars = c("None", "D0", "D1","D2","D3","D4"))
 names(data3)[2] <- "Intensity"
+names(data3)[3] <- "PercentArea"
 
 stat.desc(data)
 
-graph <- ggplot(data3, aes(fill=Intensity, y=value, x=MapDate)) + 
+graph <- ggplot(data3, aes(fill=Intensity, y=PercentArea, x=MapDate)) + 
   geom_bar(position="stack", stat="identity") + 
   scale_fill_manual(values = c("black", "yellow", "tan1", "darkorange4", "red", "rosybrown"))
 
