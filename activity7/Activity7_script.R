236c68a0b8e9ecdbd8c2d4c02a6a8c679d4327bc
guozhaosengzs <guozhaosengzs@yahoo.com>
2020-10-23 19:26:15 -0400

    Done

--- activity7/Activity7_script.R
+++ activity7/Activity7_script.R
@@ -1,19 +1,17 @@
 library(tidyverse)
 library(reshape2)
+library(anytime)
 
-data1 <- read_csv("https://raw.githubusercontent.com/guozhaosengzs/ENVDS/master/activity7/20181_2020.csv")
+data1 <- read_csv("https://raw.githubusercontent.com/guozhaosengzs/ENVDS/master/activity7/2017_2020.csv")
+data1$MapDate <- anydate(data1$MapDate)
 data2 <- data1[, c(1, 3:8)]
 data3 <- melt(data2, id.vars = "MapDate", measure.vars = c("None", "D0", "D1","D2","D3","D4"))
+names(data3)[2] <- "Intensity"
 
 stat.desc(data)
 
-graph <- ggplot(data, aes(fill=variabe, y=value, x=MapDate)) + 
-  geom_bar(position="stack", stat="identity")
+graph <- ggplot(data3, aes(fill=Intensity, y=value, x=MapDate)) + 
+  geom_bar(position="stack", stat="identity") + 
+  scale_fill_manual(values = c("black", "yellow", "tan1", "darkorange4", "red", "rosybrown"))
 
 graph
-
-
-specie <- c(rep("sorgho" , 3) , rep("poacee" , 3) , rep("banana" , 3) , rep("triticum" , 3) )
-condition <- rep(c("normal" , "stress" , "Nitrogen") , 4)
-value <- abs(rnorm(12 , 0 , 15))
-d <- data.frame(specie,condition,value)
